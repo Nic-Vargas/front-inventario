e76cfde6b9076c9ddff530bca4c015a76a99dfad
Nic-Vargas <nicolasvawi@unisabana.edu.co>
2023-11-21 18:56:23 -0500

    Agrega funcionalidad formulario

--- Agregar/Java.js
+++ Agregar/Java.js
@@ -1,13 +1,35 @@
-fetch('http://localhost:8080/producto/agregar', {
-  method: 'POST', // Método HTTP (GET, POST, etc.)
-})
-  .then(response => response.json())
-  .then(data => {
-    // Hacer algo con los datos recibidos del backend
-    console.log(data);
-  })
-  .catch(error => console.error('Error:', error));
 
+let boton = document.getElementById("btnGuardar");
+
+boton.addEventListener("click", evento=>{
+  guardarProducto();
+})
+
+
+
+
+let agregarProducto = async()=>{
+
+  let campos= {};
+
+  campos.categoria = document.getElementById("categoria").value;
+  campos.descripcion = document.getElementById("descripcion").value;
+  campos.id = document.getElementById("id").value;
+  campos.nombre = document.getElementById("nombre").value;
+  campos.precio = document.getElementById("precio").value;
+  campos.stock = document.getElementById("stock").value;
+  
+  const peticion = await fetch("http://localhost:8080/producto/agregar",
+  {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'content': 'application/json'
+    },
+    body: JSON.stringfy(campos)
+
+  });
+}
 
 
 
