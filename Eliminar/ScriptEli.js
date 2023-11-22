4f82f7014dad34cd71ac7993d3a2d15ea1c5a5d8
Nic-Vargas <nicolasvawi@unisabana.edu.co>
2023-11-22 11:55:15 -0500

    Actualiza delete

--- Eliminar/ScriptEli.js
+++ Eliminar/ScriptEli.js
@@ -5,7 +5,7 @@ boton.addEventListener("click", evento=>{
 });
 
 
-let eliminarProducto = async()=>{
+let eliminarProducto = async(id)=>{
 
   let campos = {
     id : document.getElementById("id").value,
@@ -18,7 +18,7 @@ let eliminarProducto = async()=>{
     let credentials = `${username}:${password}`;
     let encodedCredentials = btoa(credentials);
 
-    const peticion = await fetch("http://localhost:8080/producto/eliminar?id=1", {
+    const peticion = await fetch("http://localhost:8080/producto/eliminar?id=", {
       method: 'DELETE',
       headers: {
         'Accept': 'application/json',
